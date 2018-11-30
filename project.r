4469a4de02f2426be364eccc585f76bda74d3100
pvlohith <35399898+pvlohith@users.noreply.github.com>
2018-11-29 19:54:09 -0600

    Delete project.r

--- project.r
+++
@@ -1,9 +0,0 @@
-rm(list=ls(all=TRUE))
-#install.packages("haven")
-library(haven)
-library(ggplot2)
-yourData = read_dta("guns.dta")
-yourData
-summary(yourData)
-write.csv(yourData, file = "yourStataFile.csv")
-?ggplot
